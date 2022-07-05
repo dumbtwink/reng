92353ff21e7edaf871107ea2c30e2e08bd6e8f53
Alex R <alex.rooth@yahoo.com>
2022-07-05 02:14:48 +0200

    Can Create, Draw and Rotate polygons now.

--- rendereng.go
+++ rendereng.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -9,13 +8,19 @@ import (
 	"os"
 )
 
+type polygon struct {
+	p1 [2]float64
+	p2 [2]float64
+	p3 [2]float64
+}
+
 func CreateCanvas(x, y int) image.Image {
 	img := image.NewRGBA(image.Rectangle{Min: image.Point{}, Max: image.Point{X: x, Y: y}})
 	return img
 }
 
-func SaveImage(img image.Image) {
-	f, _ := os.Create("img.png")
+func SaveImage(img image.Image, dir string) {
+	f, _ := os.Create(dir)
 	defer func(f *os.File) {
 		err := f.Close()
 		if err != nil {
@@ -29,34 +34,32 @@ func SaveImage(img image.Image) {
 	}
 }
 
-func DrawCircle(img image.Image, r float64, relativeX, relativeY int, R, G, B, A uint8) {
-	var x, y float64
+func DrawCircle(img image.Image, r, x, y float64, R, G, B, A uint8) {
+	var relativex, relativey float64
 	for i := 0.0; i < 2*(r*math.Pi); i += 0.1 {
-		x = math.Cos(i) * r
-		y = math.Sin(i) * r
-		img.(*image.RGBA).Set(int(math.Ceil(x))+relativeX, int(math.Ceil(y))+relativeY, color.RGBA{R: R, G: G, B: B, A: A})
+		relativex = math.Cos(i) * r
+		relativey = math.Sin(i) * r
+		img.(*image.RGBA).Set(int(math.Ceil(relativex+x)), int(math.Ceil(relativey+y)), color.RGBA{R: R, G: G, B: B, A: A})
 	}
 }
 
 func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
 	incl := (by - ay) / (bx - ax)
-	fmt.Printf("AX: %v, AY: %v, BX: %v, BY: %v\n", ax, ay, bx, by)
-	fmt.Printf("(%v - %v) / (%v - %v) = %v\n", by, ay, bx, ax, incl)
-	fmt.Printf("Inclination %v\n", incl)
-	oct := 1
-	cnt := 1
+	var oct, cnt int
 	if ax < bx {
 		for i := ax; i < bx; i++ {
-			img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
-			fmt.Printf("(clr: %v, %v, %v, %v) X: %v, Y: %v\n", R, G, B, A, math.Ceil(i), int(float64(cnt)*incl+ax))
-			oct = cnt
-			cnt++
 			if ay < by {
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				oct = cnt
+				cnt++
 				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
 					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
 
 				}
 			} else {
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				oct = cnt
+				cnt++
 				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
 					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
 
@@ -65,16 +68,18 @@ func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
 		}
 	} else {
 		for i := ax; i > bx; i-- {
-			img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: 255, A: 255})
-			fmt.Printf("(clr: %v, %v, %v, %v) X: %v, Y: %v\n", R, G, B, A, math.Ceil(i), int(float64(cnt)*incl+ax))
-			oct = cnt
-			cnt++
 			if ay < by {
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				oct = cnt
+				cnt--
 				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
 					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
 
 				}
 			} else {
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				oct = cnt
+				cnt--
 				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
 					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
 
@@ -82,4 +87,35 @@ func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
 			}
 		}
 	}
+}
+
+func DrawPolygon(img image.Image, poly polygon, R, G, B, A uint8) {
+	DrawLine(img, poly.p1[0], poly.p1[1], poly.p2[0], poly.p2[1], R, G, B, A)
+	DrawLine(img, poly.p2[0], poly.p2[1], poly.p3[0], poly.p3[1], R, G, B, A)
+	DrawLine(img, poly.p1[0], poly.p1[1], poly.p3[0], poly.p3[1], R, G, B, A)
+}
+
+func (poly polygon) rotate(radians float64) polygon {
+	var output polygon
+	avarageX := (poly.p1[0] + poly.p2[0] + poly.p3[0]) / 3
+	avarageY := (poly.p1[1] + poly.p2[1] + poly.p3[1]) / 3
+
+	p1angle := math.Atan((poly.p1[1] - avarageY) / (poly.p1[0] - avarageX))
+	p1radius := (poly.p1[0] - avarageX) / math.Cos(p1angle)
+	relativeX := math.Cos(radians+p1angle) * p1radius
+	relativeY := math.Sin(radians+p1angle) * p1radius
+	output.p1 = [2]float64{relativeX + avarageX, relativeY + avarageY}
+
+	p2angle := math.Atan((poly.p2[1] - avarageY) / (poly.p2[0] - avarageX))
+	p2radius := (poly.p2[0] - avarageX) / math.Cos(p2angle)
+	relativeX = math.Cos(radians+p2angle) * p2radius
+	relativeY = math.Sin(radians+p2angle) * p2radius
+	output.p2 = [2]float64{relativeX + avarageX, relativeY + avarageY}
+
+	p3angle := math.Atan((poly.p3[1] - avarageY) / (poly.p3[0] - avarageX))
+	p3radius := (poly.p3[0] - avarageX) / math.Cos(p3angle)
+	relativeX = math.Cos(radians+p3angle) * p3radius
+	relativeY = math.Sin(radians+p3angle) * p3radius
+	output.p3 = [2]float64{relativeX + avarageX, relativeY + avarageY}
+	return output
 }
