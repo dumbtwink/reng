8e88391058b33c1b2998b588ce7de1a4055b44ac
Alex R <alex.rooth@yahoo.com>
2022-10-04 13:41:29 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,40 +1,46 @@
 package main
 
-import (
-	"fmt"
-)
+exi
 
 var point = [8][3]float64{{100, 100, 100}, {100, 400, 100}, {400, 400, 100}, {400, 100, 100}, {100, 100, 400}, {100, 400, 400}, {400, 400, 400}, {400, 100, 400}}
-var face1 = polygon{[64][3]float64{point[0], point[1], point[2], point[3]}, 4}
-var face2 = polygon{[64][3]float64{point[4], point[5], point[6], point[7]}, 4}
-var face3 = polygon{[64][3]float64{point[0], point[3], point[7], point[4]}, 4}
-var face4 = polygon{[64][3]float64{point[1], point[2], point[6], point[5]}, 4}
-var face5 = polygon{[64][3]float64{point[0], point[1], point[5], point[4]}, 4}
-var face6 = polygon{[64][3]float64{point[3], point[2], point[6], point[7]}, 4}
+
+var cube = mesh{polygons: [64]polygon{{vertices: [64][3]float64{point[0], point[1], point[2], point[3]}, amount: 4, clr: color.RGBA{R: 255, A: 255}},
+	{vertices: [64][3]float64{point[4], point[5], point[6], point[7]}, amount: 4, clr: color.RGBA{B: 255, A: 255}},
+	{vertices: [64][3]float64{point[0], point[3], point[7], point[4]}, amount: 4, clr: color.RGBA{G: 255, A: 255}},
+	{vertices: [64][3]float64{point[1], point[2], point[6], point[5]}, amount: 4, clr: color.RGBA{R: 255, G: 255, A: 255}},
+	{vertices: [64][3]float64{point[0], point[1], point[5], point[4]}, amount: 4, clr: color.RGBA{G: 255, B: 255, A: 255}},
+	{vertices: [64][3]float64{point[3], point[2], point[6], point[7]}, amount: 4, clr: color.RGBA{B: 255, R: 255, A: 255}}}, amount: 6}
 
 func main() {
+	donecheck := make(chan bool)
+	rendertime := make(chan int)
+	go func() {
+		for i := 0; true; i++ {
+			select {
+			case msg := <-donecheck:
+				if msg {
+					rendertime <- i
+					return
+				}
+			default:
+				time.Sleep(1000000)
+			}
+		}
+	}()
+	
 	for i := float64(0); i < 1000; i++ {
-		img := CreateCanvas(500, 500)
-		DrawPolygon(img, face1, 255, 0, 0, 255)
-		DrawPolygon(img, face2, 255, 255, 0, 255)
-		DrawPolygon(img, face3, 0, 255, 255, 255)
-		DrawPolygon(img, face4, 0, 0, 255, 255)
-		DrawPolygon(img, face5, 255, 0, 255, 255)
-		DrawPolygon(img, face6, 0, 255, 0, 255)
+		img := CreateViewport(500, 500, 500)
+		DrawMesh(img, cube)
+		DrawWireframe(img, cube, color.White)
 		var name string
 		name = fmt.Sprintf("output/%v.png", i)
 		SaveImage(img, name)
-		face1 = face1.rotate(000.1, 1, true, 250, 250, 250)
-		face2 = face2.rotate(000.1, 1, true, 250, 250, 250)
-		face3 = face3.rotate(000.1, 1, true, 250, 250, 250)
-		face4 = face4.rotate(000.1, 1, true, 250, 250, 250)
-		face5 = face5.rotate(000.1, 1, true, 250, 250, 250)
-		face6 = face6.rotate(000.1, 1, true, 250, 250, 250)
-		face1 = face1.rotate(0000.1, 2, true, 250, 250, 250)
-		face2 = face2.rotate(0000.1, 2, true, 250, 250, 250)
-		face3 = face3.rotate(0000.1, 2, true, 250, 250, 250)
-		face4 = face4.rotate(0000.1, 2, true, 250, 250, 250)
-		face5 = face5.rotate(0000.1, 2, true, 250, 250, 250)
-		face6 = face6.rotate(0000.1, 2, true, 250, 250, 250)
+		cube = cube.rotate(0.1, 0)
+		cube = cube.rotate(0.3, 1)
 	}
+	donecheck <- true
+	msg := <-rendertime
+	fmt.Printf("Done. %vms\n", msg)
+	close(donecheck)
+	close(rendertime)
 }
--- rendereng.go
+++ rendereng.go
@@ -6,15 +6,31 @@ import (
 	"image/png"
 	"math"
 	"os"
+	"sort"
 )
 
 type polygon struct {
 	vertices [64][3]float64
 	amount   int
+	clr      color.Color
 }
 
-func CreateCanvas(x, y int) image.Image {
+type mesh struct {
+	polygons [64]polygon
+	amount   int
+}
+
+type canvas struct {
+	x, y, z int
+}
+
+var viewport canvas
+
+func CreateViewport(x, y, z int) image.Image {
 	img := image.NewRGBA(image.Rectangle{Min: image.Point{}, Max: image.Point{X: x, Y: y}})
+	viewport.x = x
+	viewport.y = y
+	viewport.z = z
 	return img
 }
 
@@ -33,47 +49,47 @@ func SaveImage(img image.Image, dir string) {
 	}
 }
 
-func DrawCircle(img image.Image, r, x, y float64, R, G, B, A uint8) {
+func DrawCircle(img image.Image, r, x, y float64, clr color.Color) {
 	var relativex, relativey float64
 	for i := 0.0; i < 2*(r*math.Pi); i += 0.1 {
 		relativex = math.Cos(i) * r
 		relativey = math.Sin(i) * r
-		img.(*image.RGBA).Set(int(math.Ceil(relativex+x)), int(math.Ceil(relativey+y)), color.RGBA{R: R, G: G, B: B, A: A})
+		img.(*image.RGBA).Set(int(math.Ceil(relativex+x)), int(math.Ceil(relativey+y)), clr)
 	}
 }
 
-func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
+func DrawLine(img image.Image, ax, ay, bx, by float64, clr color.Color) {
 	incl := (by - ay) / (bx - ax)
 	var oct, cnt int
 	if math.Floor(bx)-math.Floor(ax) == 0 {
 		if ay < by {
 			for i := ay; i < by; i++ {
-				img.(*image.RGBA).Set(int(math.Ceil(ax)), int(math.Ceil(i)), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(ax)), int(math.Ceil(i)), clr)
 			}
 		} else {
 			for i := ay; i > by; i-- {
-				img.(*image.RGBA).Set(int(math.Ceil(ax)), int(math.Ceil(i)), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(ax)), int(math.Ceil(i)), clr)
 			}
 		}
 	}
 	if ax < bx {
 		for i := ax; i < bx; i++ {
 			if ay < by {
-				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), clr)
 				oct = cnt
 				cnt++
 				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
-					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
+					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), clr)
 					if j+ay > by {
 						break
 					}
 				}
 			} else {
-				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), clr)
 				oct = cnt
 				cnt++
 				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
-					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
+					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), clr)
 					if j+ay < by {
 						break
 					}
@@ -83,21 +99,21 @@ func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
 	} else {
 		for i := ax; i > bx; i-- {
 			if ay < by {
-				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), clr)
 				oct = cnt
 				cnt--
 				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
-					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
+					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), clr)
 					if j+ay > by {
 						break
 					}
 				}
 			} else {
-				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), color.RGBA{R: R, G: G, B: B, A: A})
+				img.(*image.RGBA).Set(int(math.Ceil(i)), int(float64(cnt)*incl+ay), clr)
 				oct = cnt
 				cnt--
 				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
-					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), color.RGBA{R: R, G: G, B: B, A: A})
+					img.(*image.RGBA).Set(int(math.Ceil(i)), int(math.Ceil(j)+ay), clr)
 					if j+ay < by {
 						break
 					}
@@ -107,41 +123,167 @@ func DrawLine(img image.Image, ax, ay, bx, by float64, R, G, B, A uint8) {
 	}
 }
 
-func DrawPolygon(img image.Image, poly polygon, R, G, B, A uint8) {
-	for i := 0; i < poly.amount-1; i++ {
-		DrawLine(img, poly.vertices[i][0], poly.vertices[i][1], poly.vertices[i+1][0], poly.vertices[i+1][1], R, G, B, A)
+func Trace(ax, ay, bx, by float64) [][2]float64 {
+	var coords [][2]float64
+	incl := (by - ay) / (bx - ax)
+	var oct, cnt int
+	if math.Floor(bx)-math.Floor(ax) == 0 {
+		if ay < by {
+			for i := ay; i < by; i++ {
+				coords = append(coords, [2]float64{ax, i})
+			}
+		} else {
+			for i := ay; i > by; i-- {
+				coords = append(coords, [2]float64{ax, i})
+			}
+		}
 	}
-	DrawLine(img, poly.vertices[0][0], poly.vertices[0][1], poly.vertices[poly.amount-1][0], poly.vertices[poly.amount-1][1], R, G, B, A)
+	if ax < bx {
+		for i := ax; i < bx; i++ {
+			if ay < by {
+				coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+				oct = cnt
+				cnt++
+				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
+					coords = append(coords, [2]float64{i, math.Ceil(j) + ay})
+					if j+ay > by {
+						break
+					}
+				}
+			} else {
+				coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+				oct = cnt
+				cnt++
+				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
+					coords = append(coords, [2]float64{i, math.Ceil(j) + ay})
+					if j+ay < by {
+						break
+					}
+				}
+			}
+		}
+	} else {
+		for i := ax; i > bx; i-- {
+			if ay < by {
+				coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+				oct = cnt
+				cnt--
+				for j := float64(oct) * incl; j < float64(cnt)*incl; j += 0.1 {
+					coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+					if j+ay > by {
+						break
+					}
+				}
+			} else {
+				coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+				oct = cnt
+				cnt--
+				for j := float64(oct) * incl; j > float64(cnt)*incl; j -= 0.1 {
+					coords = append(coords, [2]float64{i, float64(cnt)*incl + ay})
+					if j+ay < by {
+						break
+					}
+				}
+			}
+		}
+	}
+	return coords
 }
 
-func (poly polygon) rotate(radians float64, axis int, override bool, x, y, z float64) polygon {
+func DrawPolygon(img image.Image, poly polygon, clr color.Color) {
+	for i := 0; i < poly.amount-1; i++ {
+		DrawLine(img, poly.vertices[i][0], poly.vertices[i][1], poly.vertices[i+1][0], poly.vertices[i+1][1], clr)
+	}
+	DrawLine(img, poly.vertices[0][0], poly.vertices[0][1], poly.vertices[poly.amount-1][0], poly.vertices[poly.amount-1][1], clr)
+}
+
+func DrawWireframe(img image.Image, obj mesh, clr color.Color) {
+	newMesh := obj.SortByDepthmap()
+	for i := 0; i < obj.amount; i++ {
+		DrawPolygon(img, newMesh.polygons[i], clr)
+	}
+}
+
+func DrawMesh(img image.Image, obj mesh) {
+	newMesh := obj.SortByDepthmap()
+	for i := obj.amount - 1; i >= 0; i -= 1{
+		DrawFace(img, newMesh.polygons[i])
+	}
+}
+
+func DrawFace(img image.Image, poly polygon) {
+	for i := 0; i < poly.amount; i += 4 {
+		invlineA := Trace(poly.vertices[i][0], poly.vertices[i][1], poly.vertices[i+1][0], poly.vertices[i+1][1])
+		invlineB := Trace(poly.vertices[i+3][0], poly.vertices[i+3][1]+1, poly.vertices[i+2][0], poly.vertices[i+2][1])
+		for j := 0; j < len(invlineA) && j < len(invlineB); j += 1 {
+			DrawLine(img, invlineA[j][0], invlineA[j][1], invlineB[j][0], invlineB[j][1], poly.clr)
+		}
+	}
+
+}
+
+func (poly polygon) calcDepth() float64 {
+	var cnt float64
+	for i := 0; i < poly.amount; i++ {
+		cnt += poly.vertices[i][2]
+	}
+	return cnt / float64(poly.amount)
+}
+
+func (obj mesh) genDepthmap() []int {
+	var depthmap []int
+	for i := 0; i < obj.amount; i++ {
+		depthmap = append(depthmap, int(obj.polygons[i].calcDepth()))
+	}
+	return depthmap
+}
+
+func (obj mesh) SortByDepthmap() mesh {
+	depthmap := obj.genDepthmap()
+	var newMesh mesh
+	sort.Ints(depthmap)
+	for i := 0; i < obj.amount; i++ {
+		newMesh.polygons[i].clr = obj.polygons[i].clr
+		for j := viewport.z; j >= 0; j-- {
+			if depthmap[i] == j {
+				for k := 0; k < obj.amount; k++ {
+					if int(obj.polygons[k].calcDepth()) == depthmap[i] {
+						newMesh.polygons[i] = obj.polygons[k]
+					}
+				}
+			}
+		}
+	}
+	newMesh.amount = obj.amount
+	return newMesh
+}
+
+func (poly polygon) rotate(radians float64, axis int, obj ...mesh) polygon {
 	var output polygon
 	var xsum, ysum, avarageX, avarageY float64
 	var slotx, sloty int
+
 	switch axis {
 	case 0:
 		slotx, sloty = 0, 1
-		avarageX = x
-		avarageY = y
 		break
 	case 1:
 		slotx, sloty = 0, 2
-		avarageX = x
-		avarageY = z
 		break
 	case 2:
 		slotx, sloty = 1, 2
-		avarageX = y
-		avarageY = z
 		break
 	}
-	if !override {
-		for i := 0; i < poly.amount; i++ {
-			xsum += poly.vertices[i][slotx]
-			ysum += poly.vertices[i][sloty]
-		}
-		avarageX = (xsum) / float64(poly.amount)
-		avarageY = (ysum) / float64(poly.amount)
+	for i := 0; i < poly.amount; i++ {
+		xsum += poly.vertices[i][slotx]
+		ysum += poly.vertices[i][sloty]
+	}
+	avarageX = (xsum) / float64(poly.amount)
+	avarageY = (ysum) / float64(poly.amount)
+	if len(obj) != 0 {
+		var meshPos = obj[0].position()
+		avarageX = meshPos[slotx]
+		avarageY = meshPos[sloty]
 	}
 	for i := 0; i < poly.amount; i++ {
 		tmpangle := math.Atan((poly.vertices[i][sloty] - avarageY) / (poly.vertices[i][slotx] - avarageX))
@@ -160,6 +302,32 @@ func (poly polygon) rotate(radians float64, axis int, override bool, x, y, z flo
 			break
 		}
 	}
+	output.clr = poly.clr
 	output.amount = poly.amount
+	return output
+}
+
+func (obj mesh) position() [3]float64 {
+	var cord [3]float64
+	for i := 0; i < 3; i++ {
+		amount := 0
+		for j := 0; j < obj.amount; j++ {
+			for k := 0; k < obj.polygons[j].amount; k++ {
+				cord[i] += obj.polygons[j].vertices[k][i]
+				amount++
+			}
+		}
+		cord[i] = cord[i] / float64(amount)
+	}
+	return [3]float64{cord[0], cord[1], cord[2]}
+}
+
+func (obj mesh) rotate(radians float64, axis int) mesh {
+	var output mesh
+	for i := 0; i < obj.amount; i++ {
+		output.polygons[i] = obj.polygons[i].rotate(radians, axis, obj)
+		output.polygons[i].clr = obj.polygons[i].clr
+	}
+	output.amount = obj.amount
 	return output
 }
